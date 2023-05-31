49ad21d351f22e08eeb944ca1fb5dc0d92845cc7
mukhnaya <mosesmukhanya@gmail.com>
2023-05-31 07:14:18 +0000

    task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	parent = malloc(sizeof(binary_tree_t));
 	if (parent == NULL)
-		return NULL;
+		return (NULL);
 	parent->n = value;
 	parent->left = NULL;
 	parent->right = NULL;
